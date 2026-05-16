0560094d9942dcbf99314d5ec81cca83b86f7938
hizulu <lucia.gl.vega@gmail.com>
2026-05-16 14:03:13 +0200

    cambios min

--- FuzzyLogic_Project/Assets/Scripts/Fuzzy Logic/FuzzyAnimalController.cs
+++ FuzzyLogic_Project/Assets/Scripts/Fuzzy Logic/FuzzyAnimalController.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.AI;
 
-public class AnimalFuzzyController : MonoBehaviour
+public class FuzzyAnimalController : MonoBehaviour
 {
     public AnimalBase animal;
     public Transform player;
@@ -12,6 +12,13 @@ public class AnimalFuzzyController : MonoBehaviour
     private float distanciaActual;
     private float miedoAcumulado;
     private float curiosidadActual;
+
+    float intencion;
+
+    public float minWaitingTime = 3f;
+    public float maxWaitingTime = 7f;
+    float waitingTimer;
+    bool isWaiting=false;
 
     // Matriz que almacena la acción resultante para cada combinación [Curiosidad, Distancia, Miedo]
     private int[,,] baseReglas = new int[5, 5, 5];
@@ -28,8 +35,20 @@ public class AnimalFuzzyController : MonoBehaviour
         if (player == null) return;
 
         ActualizarVariablesEntrada();
-        float intencion = ProcesarLogicaDifusa();
-        EjecutarMovimiento(intencion);
+        intencion = ProcesarLogicaDifusa();
+
+        //Comprobar si ha llegado al destino
+        //pathPending es por si el NavMesh aún está calculando el camino
+        if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
+        {
+            if (!isWaiting) StartWait();
+        }
+
+        if (isWaiting)
+        {
+            UpdateWaiting();
+        }
+
         Debug.Log($"Animal: {animal} | Miedo: {miedoAcumulado} | Curiosidad: {curiosidadActual} | Intencion: {intencion}");
     }
 
@@ -107,7 +126,7 @@ public class AnimalFuzzyController : MonoBehaviour
         //TODO: Ajustar segun graficas
         grados[(int)TagMiedo.Relajado] = MembershipFunction.LeftShoulder(valor, 0.1f, 0.3f);
         grados[(int)TagMiedo.Cauto] = MembershipFunction.Triangle(valor, 0.2f, 0.4f, 0.6f);
-        grados[(int)TagMiedo.Alerta] = MembershipFunction.Triangle(valor, 0.4f, 0.6f, 0.8f);
+        grados[(int)TagMiedo.Alerta] = MembershipFunction.Trapezoid(valor, 0.3f, 0.45f, 0.7f, 9f);
         grados[(int)TagMiedo.Asustado] = MembershipFunction.Triangle(valor, 0.6f, 0.8f, 0.9f);
         grados[(int)TagMiedo.Panico] = MembershipFunction.RightShoulder(valor, 0.8f, 1.0f);
         return grados;
@@ -119,7 +138,7 @@ public class AnimalFuzzyController : MonoBehaviour
         //TODO: Adaptar al tamaño del juego
         grados[(int)TagDistancia.MuyCerca] = MembershipFunction.LeftShoulder(valor, 2f, 5f);
         grados[(int)TagDistancia.Cerca] = MembershipFunction.Triangle(valor, 3f, 8f, 12f);
-        grados[(int)TagDistancia.Media] = MembershipFunction.Triangle(valor, 10f, 15f, 20f);
+        grados[(int)TagDistancia.Media] = MembershipFunction.Trapezoid(valor, 10f, 15f, 20f, 25f);
         grados[(int)TagDistancia.Lejos] = MembershipFunction.Triangle(valor, 18f, 25f, 30f);
         grados[(int)TagDistancia.MuyLejos] = MembershipFunction.RightShoulder(valor, 28f, 35f);
         return grados;
@@ -130,42 +149,61 @@ public class AnimalFuzzyController : MonoBehaviour
         float[] grados = new float[5];
         grados[(int)TagCuriosidad.Nula] = MembershipFunction.LeftShoulder(valor, 0.1f, 0.3f);
         grados[(int)TagCuriosidad.Baja] = MembershipFunction.Triangle(valor, 0.2f, 0.4f, 0.6f);
-        grados[(int)TagCuriosidad.Media] = MembershipFunction.Triangle(valor, 0.4f, 0.6f, 0.8f);
+        grados[(int)TagCuriosidad.Media] = MembershipFunction.Trapezoid(valor, 0.3f, 0.5f, 0.7f, 0.9f);
         grados[(int)TagCuriosidad.Alta] = MembershipFunction.Triangle(valor, 0.6f, 0.8f, 0.9f);
         grados[(int)TagCuriosidad.Extrema] = MembershipFunction.RightShoulder(valor, 0.8f, 1.0f);
         return grados;
     }
 
-    void EjecutarMovimiento(float intencion)
+    void StartWait()
     {
-        if (Mathf.Abs(intencion) < 0.1f)
-        {
-            agent.isStopped = true;
-            animator.SetBool("IsMoving", false);
-            return;
-        }
+        isWaiting = true;
+        agent.isStopped = true;
+        agent.velocity = Vector3.zero;
 
-        agent.isStopped = false;
-        Vector3 direccion;
+        animator.SetBool("IsMoving", false);
+        waitingTimer = Random.Range(minWaitingTime, maxWaitingTime);
 
-        if (intencion > 0)
-        {
-            direccion = (player.position - transform.position).normalized;
-        }
-        else
-        {
-            direccion = (transform.position - player.position).normalized;
-        }
-
-        Vector3 destino = transform.position + direccion * 5f;
-        agent.SetDestination(destino);
-
-        agent.speed = Mathf.Abs(intencion);
-
-        ActualizarAnimacionesFuzzy(agent.speed);
+        //Elige aleatoriamente entre Idle (0)  Comer (1) mientras espera
+        animator.SetInteger("WaitType", Random.Range(0, 2));
     }
 
-    void ActualizarAnimacionesFuzzy(float speed)
+    void UpdateWaiting()
+    {
+        waitingTimer -= Time.deltaTime;
+
+        if (waitingTimer <= 0)
+        {
+            FindNewDestination(intencion);
+        }
+    }
+
+    void FindNewDestination(float intencion)
+    {
+        if(Mathf.Abs(intencion) > 0.1f)
+        {
+            agent.isStopped = false;
+            Vector3 direccion;
+
+            if (intencion > 0)
+            {
+                direccion = (player.position - transform.position).normalized;
+            }
+            else
+            {
+                direccion = (transform.position - player.position).normalized;
+            }
+
+            Vector3 destino = transform.position + direccion * 5f;
+            agent.SetDestination(destino);
+
+            agent.speed = Mathf.Abs(intencion);
+
+            UpdateAnimations(agent.speed);
+        }
+    }
+
+    void UpdateAnimations(float speed)
     {
         animator.SetBool("IsMoving", speed > 0.1f);
         animator.SetFloat("MovementSpeed", speed / 2f);
